f39c9b0c2ba14b55be9bf54394bfe601b312cdb9
Rodralp <rodrilipee@gmail.com>
2026-06-20 17:46:12 +0100

    a

--- frontend/src/services/authHeader.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-  const loginData = JSON.parse(sessionStorage.getItem('loginData') || localStorage.getItem('loginData') || 'null');
-
-  if (loginData && loginData.token) {
-    return { Authorization: 'Bearer ' + loginData.token };
-  } else {
-    return {};
-  }
-}
